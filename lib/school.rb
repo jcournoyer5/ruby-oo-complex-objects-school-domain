08bea507f71d24e49b1b18c87c5ff8a085ba1d08
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-03 20:29:42 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
     def add_student
       #  ["new_student"] = []
       #  ["new_student"] << ("AC Slater", 10)
-        school.add_student("AC Slater", 10)
+        school.add_student
         school.roster
     end
 end
