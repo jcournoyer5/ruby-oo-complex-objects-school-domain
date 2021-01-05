182e6fe00a2fc3228e095d51bac0719a1609786d
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-05 01:50:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,11 @@ class School
     def initialize(roster)
         @roster = roster
         @roster = {} 
-        @roster ["new_student"] = []
-       # @roster ["new_student"] << ("AC Slater", 10)
+        
+        @roster["AC Slater"] = 10
+       # @roster[name] = []
+      #  @roster["name"] << "AC Slater"
+       # shipping_manifest["jar of molasses"] = 10
     end   
     
     def add_student(name, grade)
@@ -31,3 +34,41 @@ class School
    # school.add_student("Screech", 11)
 
 end
+
+
+#class School
+   # attr_accessor :roster, :name, :grade
+    
+   # def initialize(school)
+    #   @school = school
+         
+#    end     
+ #   @school.new("Bayside High School")
+   # end
+    
+  #  default_hash = Hash.new
+    #default_hash.default = 1
+    
+  # def initialize(roster)
+   #     @roster = roster
+    #    @roster = {}
+   #end    
+       # roster = {}
+       #roster["new_student"] = "AC Slater"
+      
+    
+#   def add_student(name, grade)
+      # @roster ["new_student"] = []
+      
+     #   school.add_student
+    #    school.roster
+   #end
+    
+    #def grade
+    #end
+    
+   # school.add_student("AC Slater", 9)
+  #  school.add_student("Kelly Kapowski", 10)
+ #   school.add_student("Screech", 11)
+
+#end
