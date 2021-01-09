52982ce626e44c4ab91260b6373fed4a2bdc6f12
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-09 02:32:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,70 +1,33 @@
 
 
 class School
-    attr_accessor :roster
+    attr_accessor :roster, :student
   
     
     def initialize(roster)
         @roster = {} 
+        @student = student
     end
-    
     
     
     def add_student(name, grade)
-       @roster["name"] = "grade"
-      
-       # school.add_student
-      #  school.roster
+        @student.each do |name, grade|
+       @roster[name] = grade
+    end  
     end
     
     def grade(name)
-        @roster.collect do |name, grade|
-        name
+         @roster.collect do |name, grade|
+         name
+
     end
     end
     
-   # school.add_student("AC Slater", 9)
-   # school.add_student("Kelly Kapowski", 10)
-   # school.add_student("Screech", 11)
-
+    def sort
+        @roster.collect.sort do |name, grade|
+    end        
+    end
+    
 end
-
-
-
-#class School
-   # attr_accessor :roster, :name, :grade
     
-   # def initialize(school)
-    #   @school = school
-         
-#    end     
- #   @school.new("Bayside High School")
-   # end
-    
-  #  default_hash = Hash.new
-    #default_hash.default = 1
-    
-  # def initialize(roster)
-   #     @roster = roster
-    #    @roster = {}
-   #end    
-       # roster = {}
-       #roster["new_student"] = "AC Slater"
-      
-    
-#   def add_student(name, grade)
-      # @roster ["new_student"] = []
-      
-     #   school.add_student
-    #    school.roster
-   #end
-    
-    #def grade
-    #end
-    
-   # school.add_student("AC Slater", 9)
-  #  school.add_student("Kelly Kapowski", 10)
- #   school.add_student("Screech", 11)
-
-#end
 
