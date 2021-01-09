ba5f46eafb5ec386954f158236913696dc6cc535
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-09 17:17:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,33 +1,63 @@
-
+#require"pry"
 
 class School
-    attr_accessor :roster, :student
+    attr_accessor :roster
   
     
     def initialize(roster)
         @roster = {} 
-        @student = student
+        
     end
     
     
     def add_student(name, grade)
-        @student.each do |name, grade|
-       @roster[name] = grade
-    end  
+        
+        if @roster.has_key?(grade)
+          @roster[grade] << name 
+  	
+        else
+            @roster[grade] = [name] 
+        
+        end
     end
     
-    def grade(name)
-         @roster.collect do |name, grade|
-         name
+    def grade(grade)
+         @roster[grade]
+        
+         
 
-    end
     end
     
     def sort
-        @roster.collect.sort do |name, grade|
-    end        
+        @sorted_roster = {}
+        @roster.collect do |grade, name|
+            @sorted_roster[grade] = name
+        @sorted_roster[grade].sort!
     end
+      # binding.pry
+      #  @sorted_roster = @roster.sort_by {|grade, name|}name
+
+        # array.sort_by! do |item| #note the exclamation mark
+        #  item[:name]
+        # end 
+        # puts array
+       @sorted_roster.sort_by do |grade|
+           grade
+        
+    end
+    .to_h    
+        
+       
+    end
+    
+
     
 end
     
+#1) create a new, empty hash and assign it to a variable
 
+#2) iterate over the roster hash and while in there use the key/value pairs you're iterating over to make new key/value pairs in your new hash
+
+#2.5) during that same time you're adding the key/value pairs to the new hash you can also sort your arrays, the values for each key
+
+#3) at the end of the iteration the new hash should be populated with it's new key/value pairs and ready to return from this method
